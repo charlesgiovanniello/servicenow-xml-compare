aa3a4cb63fbceb1ed5c03a20d2b8138a2a8fa636
charlesgiovanniello <charles.giovanniello@gmail.com>
2024-11-04 13:51:54 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const form = document.getElementById('form');
 const baseXML = document.getElementById("baseXML");
 const customXML = document.getElementById("customXML");
+var xpathValue;
 
 //HTML fields to write back to
 const uniqueToBaseText = document.getElementById("uniqueToBase");
@@ -12,9 +13,15 @@ function parseXML(string){
     var parser = new DOMParser();
     var xmlDoc = parser.parseFromString(string, "text/xml");
     var arr = [];
-    var elements = xmlDoc.getElementsByTagName("element");
+    var elements = xmlDoc.getElementsByTagName(xpathValue);
     for(i=0;i<elements.length;i++){
-        arr.push(elements[i].childNodes[0].nodeValue);
+        try {
+            arr.push(elements[i].childNodes[0].nodeValue);
+          } catch (error) {
+            console.error(error);
+            // expected output: ReferenceError: nonExistentFunction is not defined
+            // Note - error messages will vary depending on browser
+          }
     }
     arr.sort();
     return arr;
@@ -42,6 +49,8 @@ function formatString(arr){
 }
 
 function logSubmit(event) {
+    xpathValue = document.getElementById("xpath").value;
+    console.log(xpathValue.value)
     baseFields = parseXML(baseXML.value);
     customFields = parseXML(customXML.value);
 
@@ -51,7 +60,17 @@ function logSubmit(event) {
     event.preventDefault();
 }
 
+function reset(event){
+    uniqueToBaseText.innerHTML = ""
+    uniqueToCustomText.innerHTML = ""
+    similarText.innerHTML = ""
+    form.reset()
+    
+    event.preventDefault();
+}
+
 
 form.addEventListener('submit', logSubmit);
+//form.addEventListener('click',reset)
 
 //Example Record: https://northwelltest.service-now.com/merge_form_upgrade.do?sysparm_upgrade_history_log_id=9a266094dbb2011084a5d9d96896196c&sysparm_referring_url=
